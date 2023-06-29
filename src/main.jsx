4bd49f52e279f5d29135775d4b61f0462f55d6ae
Carloslecval <carloslecval03@gmail.com>
2023-06-28 22:11:58 -0600

    removed meta and added title

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <meta
-      http-equiv="Content-Security-Policy"
-      content="upgrade-insecure-requests"
-    />
     <App />
   </React.StrictMode>
 );
