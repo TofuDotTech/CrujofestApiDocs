cc00d990201cb325945842f681937d775cf34fa8
Carloslecval <carloslecval03@gmail.com>
2023-07-27 17:51:22 -0600

    change to ip

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 function App() {
-  const url = "http://35.164.152.45:3000/static/bundled.yml";
+  const url = "http://35.91.84.215:3000/static/bundled.yml";
   return <SwaggerUI url={url} />;
 }
 
